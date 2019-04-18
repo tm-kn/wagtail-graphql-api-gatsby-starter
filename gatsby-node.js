07f91243e4116467d6b2fdcfc41cfe53cce699a5
Tomasz Knapik <hi@tmkn.org>
2019-04-18 23:00:04 +0100

    Add base fallback page type

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,38 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require('path');
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  return graphql(` {
+      wagtail {
+        pages {
+          wagtailcore {
+            page {
+              id
+              url
+              pageType
+            }
+          }
+        }
+      }
+    }
+  `).then(({ data, errors }) => {
+    if (errors) {
+      throw errors;
+    }
+
+    data.wagtail.pages.wagtailcore.page.forEach(({ url, id, pageType }) => {
+      createPage({
+        path: url,
+        component: path.resolve('src', 'pages', 'base-page.js'),
+        context: {
+          pageID: id,
+          pageType: pageType
+        }
+      });
+    });
+  });
+};
---
+++ src/pages/base-page.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql, Link } from 'gatsby';
+
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
+
+const BasePage = ({ data }) => {
+  const page = data.wagtail.pages.wagtailcore.page[0];
+
+  const renderPageList = (pages, title) => {
+    if (!pages || pages.length === 0) {
+      return;
+    }
+
+    let titleHeading;
+
+    if (title) {
+      titleHeading = <h2>{title}</h2>;
+    }
+
+    return <>
+      {titleHeading}
+      <ul>
+        {pages.map(page => <li key={page.id}>
+          <Link to={page.url}>{page.title}</Link>
+        </li>)}
+      </ul>
+    </>;
+  };
+
+  return <Layout>
+    <SEO title={page.seoTitle} description={page.seoDescription} />
+    <h1>{page.title}</h1>
+    {renderPageList([page.parent].filter(x => x), "Parent")}
+    {renderPageList(page.ancestors, "Ancestors")}
+    {renderPageList(page.children, "Children")}
+    {renderPageList(page.previousSiblings, "Previous siblings")}
+    {renderPageList(page.nextSiblings, "Next siblings")}
+    {renderPageList(page.descendants, "Descendants")}
+  </Layout>;
+};
+
+BasePage.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+
+export const query = graphql`
+  query($pageID: Int) {
+    wagtail {
+      pages {
+        wagtailcore {
+          page(id: $pageID) {
+            id
+            title
+            seoTitle
+            seoDescription
+
+            parent {
+              title
+              id
+              url
+            }
+
+            children {
+              title
+              id
+              url
+            }
+
+            previousSiblings {
+              title
+              id
+              url
+            }
+
+            nextSiblings {
+              title
+              id
+              url
+            }
+
+            ancestors {
+              id
+              title
+              url
+            }
+
+            descendants {
+              id
+              title
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default BasePage;
